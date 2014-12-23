b4d8a6d14a05d892de6dbee3680c611b0dda27ed
Erik Hvattum <erik.hvattum@gmail.com>
2014-12-24 00:39:14 +0100

    read some doc, attempting something rash!

--- setup.sh
+++ setup.sh
@@ -8,9 +8,9 @@ sudo apt-get install -y lxc-docker
 
 sudo mkdir -p /data/postgresql
 
-sudo docker run -d -e VIRTUAL_PORT=5432    -e VIRTUAL_HOST=taiga.ragesheep.com -p 5432:5432 --name postgres -v /data/postgresql:/var/lib/postgresql/data postgres
-sudo docker run -d -e VIRTUAL_PORT=8000,8080 -e VIRTUAL_HOST=taiga.ragesheep.com -p 8000:8000 -p 8080:80 --name taiga-front --link taiga-back:taiga-back ipedrazas/taiga-front
-sudo docker run -d -e VIRTUAL_PORT=8001    -e VIRTUAL_HOST=taiga.ragesheep.com -p 8001:8001 --name taiga-back --link postgres:postgres ipedrazas/taiga-back
+sudo docker run -d -e VIRTUAL_HOST=taiga.ragesheep.com -p 5432 --name postgres -v /data/postgresql:/var/lib/postgresql/data postgres
+sudo docker run -d -e VIRTUAL_PORT=80 -e VIRTUAL_HOST=taiga.ragesheep.com -p 8000 -p 80 --name taiga-front --link taiga-back:taiga-back ipedrazas/taiga-front
+sudo docker run -d -e VIRTUAL_HOST=taiga.ragesheep.com -p 8001 --name taiga-back --link postgres:postgres ipedrazas/taiga-back
 
 
 sudo docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
