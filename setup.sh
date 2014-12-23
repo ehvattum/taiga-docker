cbd7f3f4a7167e4ff3c35b3c00d5bb2384342511
Erik Hvattum <erik.hvattum@gmail.com>
2014-12-23 18:41:13 +0100

    new attempt at getting containers to run properly

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo mkdir -p /data/postgresql
 
 sudo docker run -d --name postgres		-e VIRTUAL_HOST=taiga.ragesheep.com -p 5432:5432  			-v /data/postgresql:/var/lib/postgresql/data postgres 
 sudo docker run -d --name taiga-back	-e VIRTUAL_HOST=taiga.ragesheep.com -p 8001:8001  			--link postgres:postgres ipedrazas/taiga-back
-sudo docker run -d --name taiga-front 	-e VIRTUAL_HOST=taiga.ragesheep.com -p 80:80 -p 8000:8000 	--link taiga-back:taiga-back ipedrazas/taiga-front
+sudo docker run -d --name taiga-front 	-e VIRTUAL_HOST=taiga.ragesheep.com -p 8002:80 -p 8000:8000 	--link taiga-back:taiga-back ipedrazas/taiga-front
 
 
 sudo docker run -it --link postgres:postgres --rm postgres sh -c "su postgres --command 'createuser -h "'$POSTGRES_PORT_5432_TCP_ADDR'" -p "'$POSTGRES_PORT_5432_TCP_PORT'" -d -r -s taiga'"
